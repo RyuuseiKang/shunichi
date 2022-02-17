f75329151a5bf11a45a3f523c665c3f56791c89a
RyuuseiKang <ryuuseikang@naver.com>
2022-02-17 17:37:52 +0900

    fix: 해시라우터 꼴받음

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./locale/i18n";
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
