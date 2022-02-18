cdc4cb455692b8951b319e16c1bb5d70034b8acf
RyuuseiKang <ryuuseikang@naver.com>
2022-02-18 22:18:11 +0900

    fix: 신난ㄴ다

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,130 @@ import Router from "./Router";
 
 const App: React.FC = () => {
   console.log(
+    "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMMMMMMMMMMMHBHQgaAQXpWWHWWkQQQWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMMMMM#8G+WMH9VC1?<<<<<<<<<<<<<++?7TUAdTMMMMMMMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    'MMMMMMMMMMMMMMMMMM#4JH9YC<<<<~~....~~_(~____~~____~___?"GvdMMMMMMMMMMMMMMMMMMMMM'
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMM#1d9C<~_~..__._-____<!_____~~_(-(_~~~__.`_7&TMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMM#(@<<:~_.`-_-__  .````.`_`` ._-~_~_~:~<~~~.. ?/MMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMM$d><_.._....__`` ``. ``..` _`.-` . _.__~<<~_  ((MMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMk(c:__..____~_``...`.  __.. _...`..._.-___~~_ .jMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMXkJ<_~_~_-_._.-..  _ ..-_`.  `_...__--<<((__`.jMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMuuAv(::_~_-__(~(_-. ..```.._ _..~--_((<<~~_-.gMNMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMwzzzO-<~_~~(~_.-__<____~~~~__<(<<<<(:~~~-(gMMHMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMktttOtlz-._<<(--___.___~~~~~(~~~~<((JgdH@MHMMMkzzwXpWHMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMRt==z==l=lltwwwAAkkWWWkkkXWpWHHHHHHHHMHHHMHMMMkkXWWH@HMMHMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMM@Ol???????zOwXyVpkkqqgggggggggg@@@@@@@@@HMMM#MNmaVWH@H##NN#MMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMM@O=??>;;;>?ltwXZfWbkqqmmggggggg@@@@@@H@HHH###NNMMMNmWMHHHH##MMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMM#t==?>>>;>1zltvXZfppkqqmmggmgggg@@@@H@@HHH#H#NNMMMMMMRHM@HM#MMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMM#l==??>>>+XXwOvwZVfpbkqmmmqmggg@g@@@H@@HHHHHNNNMMMMMMMRWH@@M#MMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMNrll?1z1?11ZZvwXXyVpbkqqmmmmmgg@@@@H@HH@HH##NNNMMMMMMMMHW@@M#MMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMNzOl=?=?zzzzOwXZyyfppkqqmmgggg@@@@H@@HHHHH#MNMMMMMMMMMM#g@@MMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMzvO=====zlOvXUWWfpbbWkqmmgggg@@@H@HHHHHH##NNNMMMMMMMMM#HHHMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMkzOl==lldtXkwuyfWbkqqmmgggg@@@@@HHHHHHHH#NNMNMMMMMMMMMdHHMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMM#uXwO=lldwXUwXWfbkqqmggg@@@@@@HH@H@HHHHHNNMMNMMMMMMHHQM#MMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMNwZwrtllltrUXXfbkqqmg@@@@@@@@@HHHHHHHHH#NMMMMMNMMH9XdNMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMKXXXwOttwrvuXfHkqmgg@@@@@@HH@@HHHHHHHH#MNMMMMMBUgMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMMkWyXwwrwuuXyfpkqg@@@@@@@@H@HHH@HHHHHHMMMMMSggMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMMMmXyyZuuZyyfWbqmg@@@@HHHH@HHHHHHHH#NMMNMMMMMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMMMMm?4XXZVfWpbqqg@@@@H@@HHHHHHH###NNN#MMMMMMMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMMMMMMm,?TXWWWqmg@@@HHHHHHH########MMMNMMMMMMMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMMMMMMMMNg.~?7T9HHgH@@@HHHHHHMMMMHHNMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMMMMMMMMMMMMNgJJ+++zzOwwXWWpbWHNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+  );
+  console.log(
+    "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+  );
+
+  console.log(
     "           _____                _____                    _____ "
   );
   console.log(
@@ -65,6 +189,10 @@ const App: React.FC = () => {
     "         \\:|   |                                       \\::/____/ "
   );
   console.log("          \\|___|                                        ~~ ");
+
+  console.log("気に入ったら下記のメールアドレスにお問合せください。");
+  console.log("마음에 드셨으면 아래 메일 주소로 연락 주세요.");
+  console.log("\ncontact@ryu.sh");
 
   return <Router />;
 };
